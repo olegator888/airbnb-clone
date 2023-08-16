0411d8f9b3fbcc98765abdebd50600f31faa1672
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-08-16 20:55:51 +0300

    navbar

---
+++ app/components/Container.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="mx-auto max-w-[2520px] px-4 sm:px-2 md:px-10 xl:px-20">
+      {children}
+    </div>
+  );
+};
+export default Container;
