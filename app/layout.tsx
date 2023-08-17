2ed86e0571840842c6f5405bb3d9a1ac289980aa
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-08-17 21:38:33 +0300

    register

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import Navbar from 'components/navbar/Navbar';
+import Navbar from "@/components/navbar/Navbar";
+import RegisterModal from "@/components/modals/RegisterModal";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 const font = Nunito({
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
