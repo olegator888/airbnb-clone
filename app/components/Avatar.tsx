3242bd4586277769dac6f78e997c7eebea9ab064
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-08-19 16:29:56 +0300

    github & google login

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,14 +2,18 @@
 
 import Image from "next/image";
 
-const Avatar = () => {
+interface Props {
+  src?: string | null;
+}
+
+const Avatar: React.FC<Props> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       alt="Avatar"
       height="30"
       width="30"
-      src="/images/placeholder.jpg"
+      src={src || "/images/placeholder.jpg"}
     />
   );
 };
