d41618216c8b7fbc9680a0e42ea2e590e0441fc3
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-08-20 17:46:26 +0300

    favoriting functionality

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,58 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+import { User } from "@prisma/client";
+import useLoginModal from "@/hooks/useLoginModal";
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: User | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser?.favoriteIds, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen;
+      }
+
+      try {
+        let request;
+
+        if (hasFavorited) {
+          request = () => axios.delete(`/api/favorites/${listingId}`);
+        } else {
+          request = () => axios.post(`/api/favorites/${listingId}`);
+        }
+
+        await request();
+        router.refresh();
+      } catch (e) {
+        console.log(e);
+        toast.error("Something went wrong");
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal.onOpen, router]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
